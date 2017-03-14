00ee665638fcb2807676c3148b8700ba7718a923
pratamawijaya <set.mnemonix@gmail.com>
2017-03-14 11:24:19 +0700

    revise text

--- helloworld/loopingExercise.py
+++ helloworld/loopingExercise.py
@@ -1,5 +1,7 @@
 '''
-Loop through and print out all even numbers from the numbers list in the same order they are received. Don't print any numbers that come after 237 in the sequence.
+Loop through and print out all even numbers
+from the numbers list in the same order they are received. 
+Don't print any numbers that come after 237 in the sequence.
 '''
 
 numbers = [
